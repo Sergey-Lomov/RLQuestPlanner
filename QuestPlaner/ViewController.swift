8cf08a03e83973f136838c903d113c55d52a89e2
serhii.lomov <serhii.lomov@gmail.com>
2020-01-18 23:25:02 +0200

    Initial Commit

---
+++ QuestPlaner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QuestPlaner
+//
+//  Created by Serhii.Lomov on 1/18/20.
+//  Copyright © 2020 Serhii.Lomov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
